a1fefd749598dca5ddfafebe759fa9a9a6b336eb
Frans Chokoe <fmoyagabo@gmail.com>
2025-12-18 01:24:36 +0200

    Added Roles Models

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
-from sqlalchemy import Column , Integer , String , TIMESTAMP, text
+from sqlalchemy import Column, DATETIME , Integer , String , TIMESTAMP, func, text ,ForeignKey , PrimaryKeyConstraint
+from sqlalchemy.orm import relationship
 from app.database import Base
 from datetime import timedelta
 
+# Employee Model
 class EmployeeRegister(Base):
     # table name
     __tablename__ = "employees"    
@@ -12,3 +14,13 @@ class EmployeeRegister(Base):
     age = Column(Integer , nullable=False)
     created_at = Column(TIMESTAMP(timezone=False) , server_default=text("NOW()"))
     
+    
+# Role Model
+class EmployeeRole(Base):
+    __tablename__ = "roles"
+    id = Column(Integer , nullable=False , primary_key=True , index=True)
+    position = Column(String , nullable=False)
+    started_at = Column(TIMESTAMP(timezone=True) , nullable=False , server_default=text("now()"))
+    earning = Column(Integer , nullable=False)
+    owner_id = Column(Integer , ForeignKey("employees.id" , ondelete="CASCADE") , nullable=False)
+    owner = relationship("employees")
