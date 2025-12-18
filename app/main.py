99f306bd69b3db0d811ff3b3b8d07e99b05c8cdb
Frans Chokoe <fmoyagabo@gmail.com>
2025-12-18 23:22:27 +0200

    root

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , status
 from app.database import engine
 import app.models as models
 from .routers import register , role
@@ -7,6 +7,9 @@ from .routers import register , role
 app = FastAPI()
 # models.Base.metadata.drop_all(bind=engine)
 # models.Base.metadata.create_all(bind=engine)
+@app.get("/" , status_code= status.HTTP_202_ACCEPTED)
+async def root():
+    return {"msg" : "success"}
 
 # Resgister User
 app.include_router(register.router)
